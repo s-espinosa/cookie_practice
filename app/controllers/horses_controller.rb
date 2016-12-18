39ded2b1bd9637d8b334d290cd070abbf516db10
Sal Espinosa <sespinos@gmail.com>
2016-12-18 14:22:03 -0700

    Add flash options

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -5,5 +5,18 @@ class HorsesController < ApplicationController
 
   def show
     @horse = Horse.find(params[:id])
+    if params[:flashing] == "render_now"
+      flash.now[:message] = "RENDER NOW"
+      render :show
+    elsif params[:flashing] == "render_later"
+      flash[:message] = "RENDER LATER"
+      render :show
+    elsif params[:flashing] == "redirect_now"
+      flash.now[:message] = "REDIRECT NOW"
+      redirect_to horse_path(@horse)
+    elsif params[:flashing] == "redirect_later"
+      flash[:message] = "REDIRECT LATER"
+      redirect_to horse_path(@horse)
+    end
   end
 end
